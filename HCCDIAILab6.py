07d2e3da14094547892644332bd36d70c7072758
Abdul Mateen Rahim <111953722+mateen-baloch@users.noreply.github.com>
2026-09-25 18:38:06 +0500

    Add files via upload
    For Loop and While Loop

---
+++ HCCDIAILab6.py
@@ -0,0 +1,59 @@
+# Types of Loop in Python = For loop with range & while without range
+
+# Counting with for Loop
+# for a in range(0,10):
+#     print(a)
+    
+#Table in 
+# for b in range(2,21,2):
+#     print(b)
+
+# a = input ("admin")
+# if a == "admin" :
+#     print ("True")
+# else :
+
+# for d in range (3,31,3):
+#     print (d)
+
+# e = ["Usman","Ali", "Shakoor"]
+
+# for i in e :
+
+#     print (i)
+
+# f = [1000,2,100]
+
+# for i in f :
+
+#     print (i)
+
+# # Range Program
+#     for i in range(100,110):
+
+#     print (i)
+
+# g = 2
+# while g < 20:
+#     print(g)
+#     g = g + 1
+# print ("finish")
+
+#Taught Code
+# a = 10
+# while a > 2:
+#     print (a)
+#     a = a -1
+# print ("finish")
+
+# 1. Starting Point
+# count = 1 
+
+# # 2. Condition (Jab tak count 5 ya us se chota hai)
+# while count < 5: 
+#     print("Number hai:", count)
+    
+#     # 3. Step (Har dafa count mein 1 add karo)
+#     count = count + 1 
+    
+# print("Loop khatam ho gaya!")
